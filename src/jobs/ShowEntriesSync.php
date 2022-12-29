43e6eac4f06f4ca78de8646e0cb1284300b88aa7
Cristina Zbarcea <czbarcea@pbs.org>
2022-12-29 11:36:48 -0500

    KB Archives: KB instance exclusive changes & Author: @hdytsgt

--- src/jobs/ShowEntriesSync.php
+++ src/jobs/ShowEntriesSync.php
@@ -24,6 +24,7 @@ use papertiger\mediamanager\MediaManager;
 use papertiger\mediamanager\helpers\SettingsHelper;
 use papertiger\mediamanager\helpers\SynchronizeHelper;
 
+
 class ShowEntriesSync extends BaseJob
 {
 
--- src/services/Show.php
+++ src/services/Show.php
@@ -16,6 +16,7 @@ use craft\base\Component;
 use papertiger\mediamanager\MediaManager;
 use papertiger\mediamanager\records\Show as ShowRecord;
 
+
 class Show extends Component
 {
     // Public Methods
