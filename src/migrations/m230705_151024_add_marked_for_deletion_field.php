88cbb782ba1bf5fc8df47fcdf201f59c31f58eff
Andrew Menich <andrew@simplygoodwork.com>
2023-07-13 16:38:51 -0400

    fix migration to check if schema changes have already been applied

--- src/migrations/m230705_151024_add_marked_for_deletion_field.php
+++ src/migrations/m230705_151024_add_marked_for_deletion_field.php
@@ -17,44 +17,51 @@ class m230705_151024_add_marked_for_deletion_field extends Migration
      */
     public function safeUp()
     {
-			$fieldService = Craft::$app->getFields();
-      $field = $fieldService->getFieldByHandle('markedForDeletion');
+	    $schemaVersion = Craft::$app->projectConfig->get(
+		    'plugins.mediamanager.schemaVersion',
+		    true
+	    );
+	
+	    if (version_compare($schemaVersion, '1.0.1', '<')) {
+		    $fieldService = Craft::$app->getFields();
+		    $field = $fieldService->getFieldByHandle('markedForDeletion');
+		
+		    if(!$field) {
+			    // we know this field will exist
+			    $groupToUse = $fieldService->getFieldByHandle('lastSynced')->groupId;
 			
-			if(!$field) {
-				// we know this field will exist
-				$groupToUse = $fieldService->getFieldByHandle('lastSynced')->groupId;
-				
-				$field = Craft::$app->getFields()->createField([
-					'type' => 'craft\fields\Lightswitch',
-					'name' => 'Marked for Deletion',
-					'handle' => 'markedForDeletion',
-					'groupId' => $groupToUse,
-					'searchable' => true,
-				]);
-				
-				Craft::$app->getFields()->saveField($field);
-			}
+			    $field = Craft::$app->getFields()->createField([
+				    'type' => 'craft\fields\Lightswitch',
+				    'name' => 'Marked for Deletion',
+				    'handle' => 'markedForDeletion',
+				    'groupId' => $groupToUse,
+				    'searchable' => true,
+			    ]);
 			
-			Craft::warning($field);
-			$mediaSection = Craft::$app->getSections()->getSectionByHandle(SettingsHelper::get( 'mediaSection' ));
+			    Craft::$app->getFields()->saveField($field);
+		    }
+		
+		    $mediaSection = Craft::$app->getSections()->getSectionByHandle(SettingsHelper::get( 'mediaSection' ));
+		
+		    if($mediaSection) {
+			    $entryType = $mediaSection->getEntryTypes()[0];
+			    $fieldLayout = $entryType->getFieldLayout();
+			    $allFields = $fieldLayout->getFields();
+			    $fieldLayout->setFields(ArrayHelper::merge([$field], $allFields));
+			    $tabs = $fieldLayout->getTabs();
 			
-			if($mediaSection) {
-				$entryType = $mediaSection->getEntryTypes()[0];
-				$fieldLayout = $entryType->getFieldLayout();
-				$allFields = $fieldLayout->getFields();
-				$fieldLayout->setFields(ArrayHelper::merge([$field], $allFields));
-				$tabs = $fieldLayout->getTabs();
-				
-				foreach($tabs as $tab) {
-					if ($tab->name === 'API'){
-						$existingFields = $tab->getFields();
-						$tab->setFields(ArrayHelper::merge([$field], $existingFields));
-					}
-				}
-				
-				$entryType->setFieldLayout($fieldLayout);
-				Craft::$app->getSections()->saveEntryType($entryType);
-			}
+			    foreach($tabs as $tab) {
+				    if ($tab->name === 'API'){
+					    $existingFields = $tab->getFields();
+					    $tab->setFields(ArrayHelper::merge([$field], $existingFields));
+				    }
+			    }
+			
+			    $entryType->setFieldLayout($fieldLayout);
+			    Craft::$app->getSections()->saveEntryType($entryType);
+		    }
+	    }
+			
     }
 
     /**
